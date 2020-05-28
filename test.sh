236b3769d2940c36b70a146926740d60d0a3b263
kyec1118 <61621936+kyec1118@users.noreply.github.com>
2020-05-28 16:07:43 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -17,19 +17,19 @@ R_DIR=${T_DIR}/report
 rm -rf ${R_DIR} > /dev/null 2>&1
 mkdir -p ${R_DIR}
 
-/bin/rm -f ${T_DIR}/test-plan.jtl ${T_DIR}/jmeter.log  > /dev/null 2>&1
+/bin/rm -f ${T_DIR}/get_event_history.jtl ${T_DIR}/jmeter.log  > /dev/null 2>&1
 
 ./run.sh -Dlog_level.jmeter=DEBUG \
 	-JTARGET_HOST=${TARGET_HOST} -JTARGET_PORT=${TARGET_PORT} \
 	-JTARGET_PATH=${TARGET_PATH} -JTARGET_KEYWORD=${TARGET_KEYWORD} \
-	-n -t ${T_DIR}/get_event_history.jmx -l ${T_DIR}/test-plan.jtl -j ${T_DIR}/jmeter.log \
+	-n -t ${T_DIR}/get_event_history.jmx -l ${T_DIR}/get_event_history.jtl -j ${T_DIR}/jmeter.log \
 	-e -o ${R_DIR}
 
 echo "==== jmeter.log ===="
 cat ${T_DIR}/jmeter.log
 
 echo "==== Raw Test Report ===="
-cat ${T_DIR}/test-plan.jtl
+cat ${T_DIR}/get_event_history.jtl
 
 echo "==== HTML Test Report ===="
 echo "See HTML test report in ${R_DIR}/index.html"
