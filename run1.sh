8cfc262393cce834038287cb4433d544fa254f78
kyec1118 <61621936+kyec1118@users.noreply.github.com>
2020-06-04 18:51:35 +0800

    Update run1.sh

--- run1.sh
+++ run1.sh
@@ -1,26 +1,28 @@
 #!/bin/bash
 
-echo "Enter Test Case:"
-
-read T_DIR
+T_DIR=$@
 
 FILE="$(basename ${T_DIR})"
 FILE_NAME=${FILE}$(date +"%m%d%H%M")
 
 
 echo "START Running Jmeter on `date`"
-echo "jmeter -n -t /jmeter_scripts${T_DIR}.jmx -l /Users/brooke.c/jmeter/report/${FILE_NAME}.jtl -e -o /Users/brooke.c/jmeter/report/${FILE_NAME} "
+echo "jmeter -n -t /jmeter_scripts${T_DIR}.jmx -l /report/${FILE_NAME}.jtl -e -o /report/${FILE_NAME} "
 
-sh /Users/brooke.c/jmeter/apache-jmeter-5.3/bin/jmeter.sh -n -t /Users/brooke.c/jmeter/jmeter_scripts${T_DIR}.jmx -l /Users/brooke.c/jmeter/report/${FILE_NAME}.jtl -e -o /Users/brooke.c/jmeter/report/${FILE_NAME}
+sh /opt/apache-jmeter-5.1.1/bin/jmeter.sh -n -t /jmeter_scripts${T_DIR}.jmx -l /report/${FILE_NAME}.jtl -e -o /report/${FILE_NAME}
 
 echo "END Running Jmeter on `date`"   
 
-echo "==== Raw Test Report ===="
+echo "======== Raw Test Report ========"
 
-cat /Users/brooke.c/jmeter/report/${FILE_NAME}.jtl | sed 's/,/ ,/g' | column -t -s, | less -S
+cat /report/${FILE_NAME}.jtl | sed 's/,/ ,/g' | column -t -s, | less -S
 
-echo "==== HTML Test Report ===="
+echo "See raw test report in /report/${FILE_NAME}.jtl"
 
-echo "See HTML test report in /Users/brooke.c/jmeter/report/${FILE_NAME}/index.html"
 
-open /Users/brooke.c/jmeter/report/${FILE_NAME}/index.html
+echo "======== HTML Test Report ========"
+
+echo "See HTML test report in /report/${FILE_NAME}/index.html"
+
+/bin/bash
+
